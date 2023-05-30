e7cf44d8d44285075551502f9aa76e94a9ada873
Marcos Kozelnik <makozelnik25@gmail.com>
2023-05-29 21:59:03 -0300

    entrega3

--- project/apps/merch/models.py
+++ project/apps/merch/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-
+class  ProductoCategoriaList (models.Model):
+    nombre = models.CharField (max_lenght=50)#aca estaria la extra
+    
+    def __str__(self):
+        return self.nombre
+    
 class MerchCategoria(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.CharField(max_length=250, null=True, blank=True)
